9206847d7017346e9ad032703879c3b8b8850400
Kaushal Prajapati <kaushal@techreceptives.com>
2017-12-25 11:09:48 +0530

    [IMP] 'website_partner' compatible with Multi-Website

--- addons/website_partner/models/res_partner.py
+++ addons/website_partner/models/res_partner.py
@@ -10,6 +10,11 @@ class WebsiteResPartner(models.Model):
 
     website_description = fields.Html('Website Partner Full Description', strip_style=True)
     website_short_description = fields.Text('Website Partner Short Description')
+    website_ids = fields.Many2many('website', 'website_partner_pub_rel',
+                                   'website_id', 'partner_id',
+                                   string='Websites', copy=False,
+                                   help='List of websites in which '
+                                        'Partner is published.')
 
     @api.multi
     def _compute_website_url(self):
