8529c2013a12cf9528fe14de76ace2cf6e9a0074
Kaushal Prajapati <kaushal@techreceptives.com>
2017-12-26 14:21:52 +0530

    [FIX] Remvoe required parameter from website field of link tracker

--- addons/website_links/models/link_tracker.py
+++ addons/website_links/models/link_tracker.py
@@ -7,5 +7,4 @@ class LinkTracker(models.Model):
 
     _inherit = "link.tracker"
 
-    website_id = fields.Many2one('website', string="Link For Website",
-                                 required=True)
+    website_id = fields.Many2one('website', string="Link For Website")
