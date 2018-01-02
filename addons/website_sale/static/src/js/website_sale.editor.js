23070e706c9062ec1f888d81f2e8404b2bf51125
Siddharth Bhalgami <siddharth.bhalgami@techreceptives.com>
2018-01-02 12:30:24 +0530

    [FIX] Remove Unused Product Options from Edit mode of Shop Page

--- addons/website_sale/static/src/js/website_sale.editor.js
+++ addons/website_sale/static/src/js/website_sale.editor.js
@@ -89,47 +89,6 @@ options.registry.website_sale = options.Class.extend({
             window.location.href = window.location.href.replace(/\?(enable_editor=1&)?|#.*|$/, '?enable_editor=1&');
         }
     },
-    bind_resize: function () {
-        var self = this;
-        this.$el.on('mouseenter', 'ul[name="size"] table', function (event) {
-            $(event.currentTarget).addClass("oe_hover");
-        });
-        this.$el.on('mouseleave', 'ul[name="size"] table', function (event) {
-            $(event.currentTarget).removeClass("oe_hover");
-        });
-        this.$el.on('mouseover', 'ul[name="size"] td', function (event) {
-            var $td = $(event.currentTarget);
-            var $table = $td.closest("table");
-            var x = $td.index()+1;
-            var y = $td.parent().index()+1;
-
-            var tr = [];
-            for (var yi=0; yi<y; yi++) tr.push("tr:eq("+yi+")");
-            var $select_tr = $table.find(tr.join(","));
-            var td = [];
-            for (var xi=0; xi<x; xi++) td.push("td:eq("+xi+")");
-            var $select_td = $select_tr.find(td.join(","));
-
-            $table.find("td").removeClass("select");
-            $select_td.addClass("select");
-        });
-        this.$el.on('click', 'ul[name="size"] td', function (event) {
-            var $td = $(event.currentTarget);
-            var x = $td.index()+1;
-            var y = $td.parent().index()+1;
-            self._rpc({
-                route: '/shop/change_size',
-                params: {
-                    id: self.product_tmpl_id,
-                    x: x,
-                    y: y,
-                },
-            }).then(self.reload);
-        });
-    },
-    style: function (previewMode, value, $li) {
-
-    },
     go_to: function (previewMode, value) {
         this._rpc({
             route: '/shop/change_sequence',
