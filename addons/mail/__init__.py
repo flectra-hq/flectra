c2c2f9e4fca86c4b091f3376896f17f97e99839d
Kunjal <kunjal@techreceptives.com>
2018-01-22 18:58:04 +0530

    [FIX]: Multi branch improvement.

--- addons/mail/__init__.py
+++ addons/mail/__init__.py
@@ -4,3 +4,10 @@ from . import models
 from . import wizard
 from . import controllers
 from .tests import test_mail_model
+
+
+def post_init(cr, registry):
+    from flectra import api, SUPERUSER_ID
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['publisher_warranty.contract'].update_notification()
