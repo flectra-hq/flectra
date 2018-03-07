3e52008bb1670e6b8bfe04d85ff66e298071b7ed
Siddharth Bhalgami <siddharth.bhalgami@techreceptives.com>
2018-03-07 16:59:40 +0530

    [IMP] Added default website value in Payment Gateways! As by-default every payment gateways/payment acquirer are selected/activate for default website.

--- addons/website_payment/models/payment.py
+++ addons/website_payment/models/payment.py
@@ -6,5 +6,5 @@ class PaymentAcquirer(models.Model):
     _inherit = "payment.acquirer"
 
     website_id = fields.Many2one('website', string='Website', copy=False,
-                                 help='Define website in which Payment '
-                                      'Gateway will published.')
+                                 default=lambda self: self.env.ref('website.default_website'),
+                                 help='Define website in which Payment Gateway will published.')
