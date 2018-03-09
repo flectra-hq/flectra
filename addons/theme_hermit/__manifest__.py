83b3dd675f40010579b3c2dec2deca51c3ecdae7
Mayank Vara <mayank@techreceptives.com>
2018-03-09 12:09:59 +0530

    [ADD]New Theme theme_hermit

---
+++ addons/theme_hermit/__manifest__.py
@@ -0,0 +1,14 @@
+# Part of Flectra. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Theme Hermit',
+    'description': 'Theme Hermit',
+    'category': 'Theme/Hidden',
+    'version': '1.0.0',
+    'author': 'FlectraHQ',
+    'depends': ['website'],
+    'data': [
+        'views/assets.xml',
+        'views/customize_modal.xml',
+    ],
+    'application': False,
+}
