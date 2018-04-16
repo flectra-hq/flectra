8b91e0b9c54cb4a8a80bbb8074a0cf52caa0e3dd
Gaurav Patel <gaurav@techreceptives.com>
2018-04-16 12:23:12 +0530

    [Add] Theme Leith

---
+++ addons/theme_leith/__manifest__.py
@@ -0,0 +1,14 @@
+# Part of Flectra. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Theme Leith',
+    'description': 'Theme Leith',
+    'category': 'Theme/Hidden',
+    'version': '1.0.0',
+    'author': 'FlectraHQ',
+    'depends': ['website'],
+    'data': [
+        'views/assets.xml',
+        'views/customize_modal.xml',
+    ],
+    'application': False,
+}
