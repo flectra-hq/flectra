2368ade927bdf70bc5a6acc73ddeeb25f954b452
Flectra <flectra@flectrahq.com>
2018-04-21 08:12:56 +0530

    [ADD] condition to drop table only if it exists

--- addons/website_sale/migrations/1.0.1.1/post-migrate_websites_on_pricelist.py
+++ addons/website_sale/migrations/1.0.1.1/post-migrate_websites_on_pricelist.py
@@ -3,4 +3,4 @@
 
 def migrate(cr, version):
     cr.execute("insert into website_pricelist_rule_rel (website_id,pricelist_id) select website_id,pricelist_id from website_product_pricelist")
-    cr.execute("drop table website_product_pricelist")
+    cr.execute("drop table if exists website_product_pricelist")
