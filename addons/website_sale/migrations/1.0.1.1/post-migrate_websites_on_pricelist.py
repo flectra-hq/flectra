b52ab98e3f71b224e0e79ec279b8bdb21f548096
Siddharth Bhalgami <siddharth.bhalgami@techreceptives.com>
2018-04-23 12:16:53 +0530

    Fixes https://gitlab.com/flectra-hq/flectra/issues/59

--- addons/website_sale/migrations/1.0.1.1/post-migrate_websites_on_pricelist.py
+++ addons/website_sale/migrations/1.0.1.1/post-migrate_websites_on_pricelist.py
@@ -2,5 +2,5 @@
 
 
 def migrate(cr, version):
-    cr.execute("insert into website_pricelist_rule_rel (website_id,pricelist_id) select website_id,pricelist_id from website_product_pricelist")
+    cr.execute("DO $do$ BEGIN IF EXISTS (SELECT relname FROM pg_class WHERE relname = 'website_product_pricelist') THEN INSERT INTO website_pricelist_rule_rel (website_id,pricelist_id) SELECT website_id,pricelist_id FROM website_product_pricelist; END IF; END $do$")
     cr.execute("drop table if exists website_product_pricelist")
