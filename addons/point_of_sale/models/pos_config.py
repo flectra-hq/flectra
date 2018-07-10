c8ce027ecb6a013d79ebe12c596f8fbe7d4cd180
Fatemi Lokhandwala <fatemi@techreceptives.com>
2018-07-10 12:27:47 +0530

    [ADD]:Added Upstream Patch for point_of_sale

--- addons/point_of_sale/models/pos_config.py
+++ addons/point_of_sale/models/pos_config.py
@@ -318,10 +318,11 @@ class PosConfig(models.Model):
     def name_get(self):
         result = []
         for config in self:
-            if (not config.session_ids) or (config.session_ids[0].state == 'closed'):
+            last_session = self.env['pos.session'].search([('config_id', '=', config.id)], limit=1)
+            if (not last_session) or (last_session.state == 'closed'):
                 result.append((config.id, config.name + ' (' + _('not used') + ')'))
                 continue
-            result.append((config.id, config.name + ' (' + config.session_ids[0].user_id.name + ')'))
+            result.append((config.id, config.name + ' (' + last_session.user_id.name + ')'))
         return result
 
     @api.model
@@ -350,11 +351,12 @@ class PosConfig(models.Model):
 
     @api.multi
     def write(self, vals):
-        if (self.is_posbox or vals.get('is_posbox')) and (self.iface_customer_facing_display or vals.get('iface_customer_facing_display')):
-            facing_display = (self.customer_facing_display_html or vals.get('customer_facing_display_html') or '').strip()
-            if not facing_display:
-                vals['customer_facing_display_html'] = self._compute_default_customer_html()
         result = super(PosConfig, self).write(vals)
+
+        config_display = self.filtered(lambda c: c.is_posbox and c.iface_customer_facing_display and not (c.customer_facing_display_html or '').strip())
+        if config_display:
+            super(PosConfig, config_display).write({'customer_facing_display_html': self._compute_default_customer_html()})
+
         self.sudo()._set_fiscal_position()
         self.sudo()._check_modules_to_install()
         self.sudo()._check_groups_implied()
