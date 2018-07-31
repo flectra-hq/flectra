79c899a136ae14e66f95f415426e176b7825b0b1
Renzo Meister <rm@jamotion.ch>
2018-07-31 17:31:29 +0200

    [IMP] upgrade wkhtmltopdf to 0.12.5 for flectra 1.7.x
    [ADD] stock_mrp_kit_product_short_availability
    
    gitlab CI/CD optimizations (psql12 / psql13)
    
    gitlab CI/CD optimizations
    
    gitlab CI/CD optimizations
    
    Stempeluhr PWA (WIP)
    MD5-Verschlüsselung des PIN in Python und Verprobung im Offline-Fall ebenfalls mit MD5-Vergleich
    Methoden _check_pin und _rpc_check_in_out offline verfügbar gemacht.
    Logout funktioniert aber Änderung wird noch nicht in syncCache DB gespeichert
    
    [IMP] use wkhtmltopdf 0.12.4 as 0.12.5 has memory errors
    
    Dockerfile for ubuntu and debian base images created and started with
    ubuntu_flectra Dockerfile, but not yet finished)
    
    re-enable ci stages
    
    [FIX] wkhtmltopdf error of ubuntu:bionic
    and also install libmagickwand-dev used by wand
    
    [REV] disable packaging job
    
    [IMP] use wkhtmltopdf 0.12.4 as 0.12.5 has memory errors
    
    Dockerfile for ubuntu and debian base images created and started with
    ubuntu_flectra Dockerfile, but not yet finished)
    
    re-enable ci stages
    
    [FIX] wkhtmltopdf error of ubuntu:bionic
    and also install libmagickwand-dev used by wand
    
    [REV] disable packaging job

--- setup.py
+++ setup.py
@@ -148,7 +148,7 @@ setup(
         'passlib',
         'pillow',  # windows binary http://www.lfd.uci.edu/~gohlke/pythonlibs/
         'psutil',  # windows binary code.google.com/p/psutil/downloads/list
-        'psycopg2 >= 2.2',
+        'psycopg2-binary >= 2.2',
         'pydot',
         'pyldap',  # optional
         'pyparsing',
