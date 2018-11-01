b55f10ae647ce6ae4cb20da259b4f3084620d14a
Parthiv <parthivgls@gmail.com>
2018-11-01 10:52:32 +0530

    [REL]  Flectra v1.5 Rufous

--- flectra/addons/base/__manifest__.py
+++ flectra/addons/base/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Base',
     'author': 'Odoo S.A',
-    'version': '1.4',
+    'version': '1.5',
     'category': 'Hidden',
     'description': """
 The kernel of Odoo, needed for all installation.
