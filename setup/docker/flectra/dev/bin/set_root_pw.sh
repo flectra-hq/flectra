deeac1b8158ce46a621766bf10dd6d6be97c6f5a
Renzo Meister <rm@jamotion.ch>
2020-12-26 16:10:16 +0100

    🐛 make developer container working again

--- setup/docker/flectra/dev/bin/set_root_pw.sh
+++ setup/docker/flectra/dev/bin/set_root_pw.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ -f /etc/flectra/ssh-pwd ]; then
+
+if [ -f /var/lib/flectra/.${HOSTNAME}-pwd ]; then
     echo "Root password already saved. Using this one"
-    PASS=$(</etc/flectra/ssh-pwd)
+        PASS=$(</var/lib/flectra/.${HOSTNAME}-pwd)
 else
     PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
     _word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
@@ -11,7 +12,7 @@ fi
 echo "root:$PASS" | chpasswd
 echo "flectra:$PASS" | chpasswd
 echo "=> Done!"
-echo $PASS > /etc/flectra/ssh-pwd
+echo $PASS > /var/lib/flectra/.${HOSTNAME}-pwd
 IP=$(hostname --ip-address)
 echo "========================================================================"
 echo "     You can now connect to this Developer Container via SSH using:"
@@ -23,3 +24,4 @@ echo ""
 echo "            and enter the password '$PASS' when prompted"
 echo ""
 echo "========================================================================"
+echo "§§PWD: $PASS"
