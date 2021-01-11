b2155c199126beedf951da81ed5dcef6ecee6cfa
rohit22799 <rohit@techreceptives.com>
2021-01-11 12:00:18 +0530

    [ADD] Document Management System

---
+++ addons/dms/__manifest__.py
@@ -0,0 +1,41 @@
+# Copyright 2017-2019 MuK IT GmbH
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Document Management System",
+    "summary": """Document Management System for Flectra""",
+    "version": "2.0",
+    "category": "Document Management",
+    "license": "LGPL-3",
+    "author": "FlectraHQ, MuK IT, Tecnativa, Odoo Community Association (OCA)",
+    "depends": ["mail", "http_routing", "portal"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "actions/file.xml",
+        "template/assets.xml",
+        "template/onboarding.xml",
+        "views/menu.xml",
+        "views/tag.xml",
+        "views/category.xml",
+        "views/dms_file.xml",
+        "views/directory.xml",
+        "views/storage.xml",
+        "views/dms_access_groups_views.xml",
+        "views/res_config_settings.xml",
+        "views/dms_portal_templates.xml",
+    ],
+    "qweb": ["static/src/xml/views.xml"],
+    "demo": [
+        "demo/res_users.xml",
+        "demo/access_group.xml",
+        "demo/category.xml",
+        "demo/tag.xml",
+        "demo/storage.xml",
+        "demo/directory.xml",
+        "demo/file.xml",
+        "demo/assets.xml",
+    ],
+    "images": ["static/description/banner.png"],
+    "application": True,
+}
---
+++ addons/dms/tests/test_portal.py
@@ -0,0 +1,14 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+
+import flectra.tests
+
+
+@flectra.tests.tagged("post_install", "-at_install")
+class TestDmsPortal(flectra.tests.HttpCase):
+    def test_tour(self):
+        self.phantom_js(
+            "/",
+            "flectra.__DEBUG__.services['web_tour.tour'].run('dms_portal_tour')",
+            "flectra.__DEBUG__.services['web_tour.tour'].tours.dms_portal_tour.ready",
+            login="portal",
+        )
