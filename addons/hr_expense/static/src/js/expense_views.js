6957509c414a9b18649b34ac2686272d23ff11c1
Parthiv Patel <parthiv@techreceptives.com>
2021-07-09 06:06:14 +0000

    [FIX] app id

--- addons/hr_expense/static/src/js/expense_views.js
+++ addons/hr_expense/static/src/js/expense_views.js
@@ -29,12 +29,12 @@ flectra.define('hr_expense.expenses.tree', function (require) {
             const self = this;
             await this._super(...arguments);
             const google_url = "https://play.google.com/store/apps/details?id=com.flectra.mobile";
-            const apple_url = "https://apps.apple.com/be/app/flectra/id1272543640";
+            const apple_url = "https://apps.apple.com/be/app/flectra/id1561830563";
             const action_desktop = {
                 name: 'Download our App',
                 type: 'ir.actions.client',
                 tag: 'expense_qr_code_modal',
-                params: {'url': "https://apps.apple.com/be/app/flectra/id1272543640"},
+                params: {'url': "https://apps.apple.com/be/app/flectra/id1561830563"},
                 target: 'new',
             };
             this.$el.find('img.o_expense_apple_store').on('click', function(event) {
