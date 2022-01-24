8b718c13a5be3683857d64bba79ec2947f7e7553
Thomas Winteler <info@win-soft.ch>
2022-01-24 15:03:52 +0000

    Master account chqr fix

--- addons/account_bank_statement_import/models/__init__.py
+++ addons/account_bank_statement_import/models/__init__.py
@@ -2,6 +2,8 @@
 # Part of Odoo, Flectra. See LICENSE file for full copyright and licensing details.
 
 from . import account_bank_statement_line
+from . import account_bank_statement_import
+from . import account_journal
 from . import res_company
 from . import account_bank_statement
 from . import account_journal_dashboard
