dab9f59649ae44d9781d9c41f74d94903062d6aa
Parthiv Patel <parthiv@techreceptives.com>
2022-01-30 08:34:55 +0000

    [PATCH] Upstream patch - 30012022

--- addons/google_gmail/__init__.py
+++ addons/google_gmail/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# Part of Flectra, Flectra. See LICENSE file for full copyright and licensing details.
+# Part of Odoo, Flectra. See LICENSE file for full copyright and licensing details.
 
 from . import models
