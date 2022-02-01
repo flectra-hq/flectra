6ac21662a5d738c3a51698e86fae2c5f03a6dc34
Parthiv Patel <parthiv@techreceptives.com>
2022-02-01 08:36:03 +0000

    [PATCH] Upstream patch - 01022022

--- addons/website_blog/tests/test_ui.py
+++ addons/website_blog/tests/test_ui.py
@@ -2,13 +2,15 @@
 # Part of Odoo, Flectra. See LICENSE file for full copyright and licensing details.
 
 import flectra.tests
-from flectra import tools
 
 
 @flectra.tests.tagged('post_install', '-at_install')
 class TestUi(flectra.tests.HttpCase):
     def test_admin(self):
+        # Ensure at least two blogs exist for the step asking to select a blog
         self.env['blog.blog'].create({'name': 'Travel'})
+
+        # Ensure at least one image exists for the step that chooses one
         self.env['ir.attachment'].create({
             'public': True,
             'type': 'url',
@@ -16,4 +18,5 @@ class TestUi(flectra.tests.HttpCase):
             'name': 'transparent.png',
             'mimetype': 'image/png',
         })
+
         self.start_tour("/", 'blog', login='admin')
