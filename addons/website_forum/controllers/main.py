5e72a9cdc848807538d64ae248fd372b4de9042c
Parthiv Patel <parthiv@techreceptives.com>
2022-04-20 08:35:35 +0000

    [PATCH] Upstream patch - 20042022

--- addons/website_forum/controllers/main.py
+++ addons/website_forum/controllers/main.py
@@ -16,6 +16,7 @@ from flectra.addons.website.models.ir_http import sitemap_qs2dom
 from flectra.addons.website_profile.controllers.main import WebsiteProfile
 from flectra.addons.portal.controllers.portal import _build_url_w_params
 
+from flectra.exceptions import UserError
 from flectra.http import request
 
 _logger = logging.getLogger(__name__)
@@ -122,8 +123,9 @@ class WebsiteForum(WebsiteProfile):
             # check that sorting is valid
             # retro-compatibily for V8 and google links
             try:
+                sorting = werkzeug.urls.url_unquote_plus(sorting)
                 Post._generate_order_by(sorting, None)
-            except ValueError:
+            except (UserError, ValueError):
                 sorting = False
 
         if not sorting:
