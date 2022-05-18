22cb5757f682d78e5d4b035fa3e54e313d3a447f
Parthiv Patel <parthiv@techreceptives.com>
2022-05-18 08:34:38 +0000

    [PATCH] Upstream patch - 18052022

--- addons/microsoft_calendar/utils/microsoft_event.py
+++ addons/microsoft_calendar/utils/microsoft_event.py
@@ -3,16 +3,17 @@
 from flectra.api import model
 from typing import Iterator, Mapping
 from collections import abc
+from flectra.addons.microsoft_calendar.utils.event_id_storage import combine_ids
 
 
 class MicrosoftEvent(abc.Set):
-    """This helper class holds the values of a Microsoft event.
+    """
+    This helper class holds the values of a Microsoft event.
     Inspired by Flectra recordset, one instance can be a single Microsoft event or a
     (immutable) set of Microsoft events.
     All usual set operations are supported (union, intersection, etc).
 
     :param iterable: iterable of MicrosoftCalendar instances or iterable of dictionnaries
-
     """
 
     def __init__(self, iterable=()):
@@ -51,86 +52,108 @@ class MicrosoftEvent(abc.Set):
 
     @property
     def ids(self):
+        """
+        Use 'id' to return an event identifier which is specific to a calendar
+        """
         return tuple(e.id for e in self)
 
     def microsoft_ids(self):
         return tuple(e.id for e in self)
 
+    @property
+    def uids(self):
+        """
+        Use 'iCalUid' to return an identifier which is unique accross all calendars
+        """
+        return tuple(e.iCalUId for e in self)
+
     def flectra_id(self, env):
-        self.flectra_ids(env)  # load ids
         return self._flectra_id
 
     def _meta_flectra_id(self, microsoft_guid):
         """Returns the Flectra id stored in the Microsoft Event metadata.
         This id might not actually exists in the database.
         """
-        if self.singleValueExtendedProperties:
-            o_id = [prop['value'] for prop in self.singleValueExtendedProperties if prop['id'] == 'String {%s} Name flectra_id' % microsoft_guid][0]
-            return int(o_id)
+        return None
 
-    def flectra_ids(self, env):
-        ids = tuple(e._flectra_id for e in self if e._flectra_id)
-        if len(ids) == len(self):
-            return ids
-        found = self._load_flectra_ids_from_db(env)
-        unsure = self - found
-        if unsure:
-            unsure._load_flectra_ids_from_metadata(env)
+    @property
+    def flectra_ids(self):
+        """
+        Get the list of Flectra event ids already mapped with Outlook events (self)
+        """
+        return tuple(e._flectra_id for e in self if e._flectra_id)
 
-        return tuple(e._flectra_id for e in self)
+    def _load_flectra_ids_from_db(self, env, force_model=None):
+        """
+        Map Microsoft events to existing Flectra events:
+        1) extract unmapped events only,
+        2) match Flectra events and Outlook events which have both a ICalUId set,
+        3) match remaining events,
+        Returns the list of mapped events
+        """
+        mapped_events = [e.id for e in self if e._flectra_id]
 
-    def _load_flectra_ids_from_metadata(self, env):
-        model_env = self._get_model(env)
-        microsoft_guid = env['ir.config_parameter'].sudo().get_param('microsoft_calendar.microsoft_guid', False)
-        unsure_flectra_ids = tuple(e._meta_flectra_id(microsoft_guid) for e in self)
-        flectra_events = model_env.browse(_id for _id in unsure_flectra_ids if _id)
+        # avoid mapping events if they are already all mapped
+        if len(self) == len(mapped_events):
+            return self
 
-        # Extended properties are copied when splitting a recurrence Microsoft side.
-        # Hence, we may have two Microsoft recurrences linked to the same Flectra id.
-        # Therefore, we only consider Flectra records without microsoft id when trying
-        # to match events.
-        o_ids = flectra_events.exists().filtered(lambda e: not e.microsoft_id).ids
-        for e in self:
-            flectra_id = e._meta_flectra_id(microsoft_guid)
-            if flectra_id in o_ids:
-                e._events[e.id]['_flectra_id'] = flectra_id
+        unmapped_events = self.filter(lambda e: e.id not in mapped_events)
 
-    def _load_flectra_ids_from_db(self, env):
-        model_env = self._get_model(env)
-        flectra_events = model_env.with_context(active_test=False)._from_microsoft_ids(self.ids).with_env(env)
-        mapping = {e.microsoft_id: e.id for e in flectra_events}
-        existing_microsoft_ids = flectra_events.mapped('microsoft_id')
-        for e in self:
-            flectra_id = mapping.get(e.id)
+        model_env = force_model if force_model is not None else self._get_model(env)
+        flectra_events = model_env.with_context(active_test=False).search([
+            '|',
+            ('ms_universal_event_id', "in", unmapped_events.uids),
+            ('ms_organizer_event_id', "in", unmapped_events.ids)
+        ]).with_env(env)
+
+        # 1. try to match unmapped events with Flectra events using their iCalUId
+        unmapped_events_with_uids = unmapped_events.filter(lambda e: e.iCalUId)
+        flectra_events_with_uids = flectra_events.filtered(lambda e: e.ms_universal_event_id)
+        mapping = {e.ms_universal_event_id: e.id for e in flectra_events_with_uids}
+
+        for ms_event in unmapped_events_with_uids:
+            flectra_id = mapping.get(ms_event.iCalUId)
             if flectra_id:
-                e._events[e.id]['_flectra_id'] = flectra_id
-        return self.filter(lambda e: e.id in existing_microsoft_ids)
+                ms_event._events[ms_event.id]['_flectra_id'] = flectra_id
+                mapped_events.append(ms_event.id)
 
-    def owner(self, env):
-        # Owner/organizer could be desynchronised between Microsoft and Flectra.
-        # Let userA, userB be two new users (never synced to Microsoft before).
-        # UserA creates an event in Flectra (he is the owner) but userB syncs first.
-        # There is no way to insert the event into userA's calendar since we don't have
-        # any authentication access. The event is therefore inserted into userB's calendar
-        # (he is the orginizer in Microsoft). The "real" owner (in Flectra) is stored as an
-        # extended property. There is currently no support to "transfert" ownership when
-        # userA syncs his calendar the first time.
-        if self.singleValueExtendedProperties:
-            microsoft_guid = env['ir.config_parameter'].sudo().get_param('microsoft_calendar.microsoft_guid', False)
-            real_owner_id = [prop['value'] for prop in self.singleValueExtendedProperties if prop['id'] == 'String {%s} Name owner_flectra_id' % microsoft_guid][0]
-            real_owner = real_owner_id and env['res.users'].browse(int(real_owner_id))
-        else:
-            real_owner_id = False
+        # 2. try to match unmapped events with Flectra events using their id
+        unmapped_events = self.filter(lambda e: e.id not in mapped_events)
+        mapping = {e.ms_organizer_event_id: e for e in flectra_events}
 
-        if real_owner_id and real_owner.exists():
-            return real_owner
-        elif self.isOrganizer:
-            return env.user
-        elif self.organizer and self.organizer.get('emailAddress') and self.organizer.get('emailAddress').get('address'):
-            # In Microsoft: 1 email = 1 user; but in Flectra several users might have the same email
-            return env['res.users'].search([('email', '=', self.organizer.get('emailAddress').get('address'))], limit=1)
-        else:
-            return env['res.users']
+        for ms_event in unmapped_events:
+            flectra_event = mapping.get(ms_event.id)
+            if flectra_event:
+                ms_event._events[ms_event.id]['_flectra_id'] = flectra_event.id
+                mapped_events.append(ms_event.id)
+
+                # don't forget to also set the global event ID on the Flectra event to ease
+                # and improve reliability of future mappings
+                flectra_event.write({
+                    'microsoft_id': combine_ids(ms_event.id, ms_event.iCalUId),
+                    'need_sync_m': False,
+                })
+
+        return self.filter(lambda e: e.id in mapped_events)
+
+    def owner_id(self, env):
+        """
+        Indicates who is the owner of an event (i.e the organizer of the event).
+
+        There are several possible cases:
+        1) the current Flectra user is the organizer of the event according to Outlook event, so return his id.
+        2) the current Flectra user is NOT the organizer and:
+           2.1) we are able to find a Flectra user using the Outlook event organizer email address and we use his id,
+           2.2) we are NOT able to find a Flectra user matching the organizer email address and we return False, meaning
+                that no Flectra user will be able to modify this event. All modifications will be done from Outlook.
+        """
+        if self.isOrganizer:
+            return env.user.id
+        if self.organizer.get('emailAddress') and self.organizer.get('emailAddress').get('address'):
+            # Warning: In Microsoft: 1 email = 1 user; but in Flectra several users might have the same email
+            user = env['res.users'].search([('email', '=', self.organizer.get('emailAddress').get('address'))], limit=1)
+            return user.id if user else False
+        return False
 
     def filter(self, func) -> 'MicrosoftEvent':
         return MicrosoftEvent(e for e in self if func(e))
@@ -191,6 +214,7 @@ class MicrosoftEvent(abc.Set):
         if month_by:
             result['month_by'] = month_by
 
+        # daysOfWeek contains the full name of the day, the fields contain the first 2 letters (mo, tu, etc)
         week_days = [x[:2] for x in pattern.get('daysOfWeek', [])]
         for week_day in ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su']:
             result[week_day] = week_day in week_days
@@ -199,21 +223,32 @@ class MicrosoftEvent(abc.Set):
         return result
 
     def is_cancelled(self):
-        return bool(self.isCancelled or (self.__getattr__('@removed') and self.__getattr__('@removed').get('reason') == 'deleted'))
+        return bool(self.isCancelled) or self.is_removed()
+
+    def is_removed(self):
+        return self.__getattr__('@removed') and self.__getattr__('@removed').get('reason') == 'deleted'
 
     def is_recurrence_outlier(self):
-        return bool(self.originalStartTime)
+        return self.type == "exception"
 
     def cancelled(self):
         return self.filter(lambda e: e.is_cancelled())
 
-    def exists(self, env) -> 'MicrosoftEvent':
-        recurrences = self.filter(MicrosoftEvent.is_recurrence)
-        events = self - recurrences
-        recurrences.flectra_ids(env)
-        events.flectra_ids(env)
+    def match_with_flectra_events(self, env) -> 'MicrosoftEvent':
+        """
+        Match Outlook events (self) with existing Flectra events, and return the list of matched events
+        """
+        # first, try to match recurrences
+        # Note that when a recurrence is removed, there is no field in Outlook data to identify
+        # the item as a recurrence, so select all deleted items by default.
+        recurrence_candidates = self.filter(lambda x: x.is_recurrence() or x.is_removed())
+        mapped_recurrences = recurrence_candidates._load_flectra_ids_from_db(env, force_model=env["calendar.recurrence"])
 
-        return self.filter(lambda e: e._flectra_id)
+        # then, try to match events
+        events_candidates = (self - mapped_recurrences).filter(lambda x: not x.is_recurrence())
+        mapped_events = events_candidates._load_flectra_ids_from_db(env)
+
+        return mapped_recurrences | mapped_events
 
     def _get_model(self, env):
         if all(e.is_recurrence() for e in self):
