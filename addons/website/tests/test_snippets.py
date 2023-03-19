90effe3bad7c1c71d773e97b47e2674b464b4b30
Parthiv Patel <parthiv@techreceptives.com>
2023-03-19 08:34:17 +0000

    [PATCH] Upstream patch - 19032023

--- addons/website/tests/test_snippets.py
+++ addons/website/tests/test_snippets.py
@@ -3,6 +3,8 @@
 
 import flectra
 import flectra.tests
+from flectra.tests.common import HOST
+from flectra.tools import config
 
 
 @flectra.tests.common.tagged('post_install', '-at_install', 'website_snippets')
@@ -13,3 +15,20 @@ class TestSnippets(flectra.tests.HttpCase):
 
     def test_02_countdown_preview(self):
         self.start_tour("/?enable_editor=1", "snippet_countdown", login='admin')
+
+    def test_03_snippet_image_gallery(self):
+        IrAttachment = self.env['ir.attachment']
+        base = "http://%s:%s" % (HOST, config['http_port'])
+        IrAttachment.create({
+            'public': True,
+            'name': 's_default_image.jpg',
+            'type': 'url',
+            'url': base + '/web/image/website.s_banner_default_image.jpg',
+        })
+        IrAttachment.create({
+            'public': True,
+            'name': 's_default_image2.jpg',
+            'type': 'url',
+            'url': base + '/web/image/website.s_banner_default_image.jpg',
+        })
+        self.start_tour("/", "snippet_image_gallery", login='admin')
