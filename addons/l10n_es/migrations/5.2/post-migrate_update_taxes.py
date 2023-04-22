0fa73828f2d5fb7f5d57a583f7889a6146e68e5c
Parthiv Patel <parthiv@techreceptives.com>
2023-04-22 08:34:34 +0000

    [PATCH] Upstream patch - 22042023

---
+++ addons/l10n_es/migrations/5.2/post-migrate_update_taxes.py
@@ -0,0 +1,7 @@
+# Part of Odoo, Flectra. See LICENSE file for full copyright and licensing details.
+
+from flectra.addons.account.models.chart_template import update_taxes_from_templates
+
+
+def migrate(cr, version):
+    update_taxes_from_templates(cr, 'l10n_es.account_chart_template_common')
