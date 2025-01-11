d655d3bbfbbca99587d6abc91199dbde2b7eef0a
parthiv patel <parthivgls@gmail.com>
2025-01-11 09:40:40 +0000

    [PATCH] upstream

---
+++ addons/project_todo/static/src/views/todo_form/todo_form_status_bar_button.js
@@ -0,0 +1,6 @@
+/* @flectra-module */
+import { StatusBarButtons } from "@web/views/form/status_bar_buttons/status_bar_buttons";
+
+export class TodoFormStatusBarButtons extends StatusBarButtons {
+    static template = "project_todo.TodoFormStatusBarButtons";
+}
