f710a04a12aa63c3f4216ae1ee02d195efd762c4
parthiv patel <parthivgls@gmail.com>
2025-01-16 07:05:42 +0000

    [PATCH] upstream

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -15,6 +15,7 @@ from textwrap import shorten
 from flectra import api, fields, models, _, Command, SUPERUSER_ID, modules, tools
 from flectra.addons.account.tools import format_structured_reference_iso
 from flectra.exceptions import UserError, ValidationError, AccessError, RedirectWarning
+from flectra.tools.misc import clean_context
 from flectra.tools import (
     date_utils,
     email_re,
@@ -2319,7 +2320,7 @@ class AccountMove(models.Model):
         if to_delete:
             self.env['account.move.line'].browse(to_delete).with_context(dynamic_unlink=True).unlink()
         if to_create:
-            self.env['account.move.line'].create([
+            self.env['account.move.line'].with_context(clean_context(self.env.context)).create([
                 {**key, **values, 'display_type': line_type}
                 for key, values in to_create.items()
             ])
